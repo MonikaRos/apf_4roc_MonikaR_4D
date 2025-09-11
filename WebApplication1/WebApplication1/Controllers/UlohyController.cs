13251243333b0715159e1ad6c6eb52330ba71f75
Monika Rosivalová <monika.rosivalova1@gmail.com>
2025-09-11 07:42:04 +0200

    uloha1

---
+++ WebApplication1/WebApplication1/Controllers/UlohyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UlohyController : Controller
+    {
+        public IActionResult Uloha1()
+        {
+            return View();
+        }
+
+        public IActionResult Uloha2()
+        {
+            return View();
+        }
+    }
+}
